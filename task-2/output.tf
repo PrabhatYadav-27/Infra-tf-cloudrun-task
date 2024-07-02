1a2fb9d99537fc2574e7b57bfa3c9907c64f3a51
Prabhat Yadav <99250179+PrabhatYadav-27@users.noreply.github.com>
2024-07-02 12:13:25 +0530

    Update output.tf

--- task-2/output.tf
+++ task-2/output.tf
@@ -2,13 +2,3 @@ output "cloud_run_url" {
   value       = module.cloud_run.cloud_run_url # will display the url of the cloud run
   description = "The URL of Cloud Run service"
 }
-
-# output "vpc_self_link" {
-#   value       = module.vpc.vpc_self_link
-#   description = "The self link of the vpc"
-# }
-
-# output "subnets_self_link" {
-#   value       = module.subnet[*].subnets_self_link
-#   description = "The self link of the subnet"
-# }
